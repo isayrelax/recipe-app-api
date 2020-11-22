6fbc0420707177762548cbe5f7e6d1b4cf75958c
fp <fp@Joeys-iMac.local>
2020-11-22 13:34:53 -0500

    Add list recipe feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 
 
 class TagSerializer(serializers.ModelSerializer):
-    '''Serializer for tag objects'''
+    """Serializer for tag objects"""
 
     class Meta:
         model = Tag
@@ -18,4 +18,22 @@ class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
         fields = ('id', 'name')
+        read_only_fields = ('id',)
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serialize a recipe"""
+    ingredients = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Ingredient.objects.all()
+    )
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Tag.objects.all()
+    )
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes',
+                  'price', 'link')
         read_only_fields = ('id',)
